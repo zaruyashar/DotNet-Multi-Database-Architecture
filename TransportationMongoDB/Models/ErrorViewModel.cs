dd2d1f1192cc749a0ce153e7cbb868b878dd97bb
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-03-24 22:05:35 +0300

    feat: Add initial project structures for MSSQL, PostgreSQL, and MongoDB

---
+++ TransportationMongoDB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TransportationMongoDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
