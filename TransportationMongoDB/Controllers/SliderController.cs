03a4c82677c96757b667f19fe696ccba26cd9738
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-04-01 01:26:38 +0300

    coursework in progress: 9% complete

---
+++ TransportationMongoDB/Controllers/SliderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransportationMongoDB.Controllers
+{
+    public class SliderController : Controller
+    {
+        public IActionResult SliderList()
+        {
+            return View();
+        }
+    }
+}
---
+++ TransportationMongoDB/ViewComponents/AdminComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TransportationMongoDB.ViewComponents.AdminComponents
+{
+    public class _AdminLayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
