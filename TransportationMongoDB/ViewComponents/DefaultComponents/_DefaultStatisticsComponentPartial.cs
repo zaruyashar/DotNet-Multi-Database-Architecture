aad09727fad725566f5b8372258267ff928b198a
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-04-11 03:11:12 +0300

    coursework in progress: 48% complete

--- TransportationMongoDB/ViewComponents/DefaultComponents/_DefaultStatisticsComponentPartial.cs
+++ TransportationMongoDB/ViewComponents/DefaultComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TransportationMongoDB.Services.ShipmentServices;
 
 namespace TransportationMongoDB.ViewComponents.DefaultComponents
 {
     public class _DefaultStatisticsComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IShipmentService _shipmentService;
+
+        public _DefaultStatisticsComponentPartial(IShipmentService shipmentService)
         {
+            _shipmentService = shipmentService;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.v1 = await _shipmentService.GetTotalShipmentCountAsync();
+            ViewBag.v2 = await _shipmentService.GetDeliveredShipmentCountAsync();
+            ViewBag.v3 = await _shipmentService.GetDistinctDestinationCityCountAsync();
+            ViewBag.v4 = await _shipmentService.GetInDistributionShipmentCountAsync();
             return View();
         }
     }
