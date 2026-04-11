f57680d941b7f99a7639e037aa523c5d382e69d0
Nejla Nur Yaşar <203256006+zaruyashar@users.noreply.github.com>
2026-04-11 17:01:46 +0300

    chore: isolate MongoDB project and remove unused templates

--- RestaurantPostgreSQL/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RestaurantPostgreSQL.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
